eade679fe19e196b4cf60cdd5beaffb98cef56e2
rabi-design <snowsnow123@ezweb.ne.jp>
2021-10-15 20:47:00 +0900

    Changes

--- other/setup.py
+++ other/setup.py
@@ -3,7 +3,7 @@ from selenium.webdriver.support import expected_conditions as EC
 from selenium.webdriver.support.ui import WebDriverWait
 from selenium.webdriver.common.by import By
 from time import sleep
-from scrape.bs_req import bs_req
+from soup import Soup
 from webdriver_manager.utils import chrome_version
 import os, glob, shutil, subprocess, zipfile
 import pyautogui as pg
@@ -20,7 +20,7 @@ def help_s():
     elif ver == "93":
         n = 2
 
-    li, no = bs_req("https://chromedriver.chromium.org/downloads")
+    li = Soup.bs("https://chromedriver.chromium.org/downloads")
     lis = li.find_all("li", class_="TYR86d wXCUfe zfr3Q")
     down = lis[n].find("a", class_="XqQF9c").get("href")
     print(down)
@@ -46,8 +46,8 @@ def start_chrome(path):
 
     options.add_argument("start-maximized")
     options.add_argument('--user-data-dir={0}'.format(path))
-    driver = webdriver.Chrome(options = options,
-                              executable_path = "extensions/chromedriver.exe")
+    driver = webdriver.Chrome(options=options,
+                              executable_path="extensions/chromedriver.exe")
     driver.maximize_window()  # 画面サイズ最大化
 
     # GoogleログインURL
@@ -84,8 +84,22 @@ def login_google(driver):
     driver.find_element_by_xpath(login_pw_xpath).click()
 
 
+def gfp_option(driver):
+    driver.get("chrome-extension://fdpohaocaechififmbbbbbknoalclacl/options.html")
+    sleep(5)
+    driver.find_elements_by_class_name("options-input")[6].click()
+    # driver.execute_script('return document.querySelectorAll(".options-input")[6].click()')
+    sleep(2)
+    pg.hotkey("left")
+    sleep(1)
+    pg.hotkey("enter")
+    sleep(1)
+    driver.quit()
+
+
 def expand_shadow_element_gfp(driver):
-    driver.get("https://chrome.google.com/webstore/detail/gofullpage-full-page-scre/fdpohaocaechififmbbbbbknoalclacl?hl=ja")
+    driver.get(
+        "https://chrome.google.com/webstore/detail/gofullpage-full-page-scre/fdpohaocaechififmbbbbbknoalclacl?hl=ja")
     sleep(5)
     driver.execute_script('return document'
                           '.querySelector(".g-c-x").click()')
@@ -147,10 +161,12 @@ def expand_shadow_element_ext(driver, j):
     #                       '.shadowRoot.querySelector("cr-dialog").querySelector("[slot={0}]").querySelector(".action-button").click()'.format(a))
 
 
+# noinspection PyBroadException
 def main():
     u_name = os.getlogin()
     ud_path = 'C:\\Users\\{0}\\AppData\\Local\\Google\\Chrome\\User Data'.format(u_name)
     driver2 = start_chrome(ud_path)
+
     expand_shadow_element_gfp(driver2)
     sleep(10)
     driver2.quit()
@@ -163,7 +179,7 @@ def main():
     driver2 = start_chrome(ud_path)
     for i in gfp_path:
         expand_shadow_element_ext(driver2, i)
-
+    gfp_option(driver2)
     crx_path = glob.glob(os.path.join(ud_path, "Default\Extensions", gfp_id, "*.crx"))
     for i in crx_path:
         shutil.copyfile(i, "extensions\{0}".format(os.path.basename(i)))
@@ -180,7 +196,7 @@ def main():
 
 if __name__ == "__main__":
     help_s()
-    # nfreez()
-    # main()
+    nfreez()
+    main()
     # Googleにログイン
     # login_google(driver2)
